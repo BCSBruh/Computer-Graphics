ed158ae0278bfed1267420dcd548927bee2b199e
BCSBruh <87160862+BCSBruh@users.noreply.github.com>
2023-02-02 19:55:03 -0500

    Fixing stuff

---
+++ assignment2/Assignment2.java
@@ -0,0 +1,18 @@
+package assignment2;
+
+import java.awt.*;
+
+public class Assignment2 {
+    public static Shape createYingYang() {
+        int radius = 100;
+        int diameter = radius*2;
+
+
+
+
+    }
+
+    public static Shape createSpirograph() {
+        return null;
+    }
+}
