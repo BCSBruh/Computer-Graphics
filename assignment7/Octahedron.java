81951545a359577e07bd57d71b05f6c05b024042
BCSBruh <jeromelarson26@gmail.com>
2023-03-31 10:29:36 -0400

    Finished Assignment 7

--- assignment7/Octahedron.java
+++ assignment7/Octahedron.java
@@ -6,12 +6,14 @@ import javax.vecmath.Point3f;
 
 /**
  *
- * @author 
+ * @author Jerome Larson
  */
 public class Octahedron extends IndexedTriangleArray {
 
     public Octahedron() {
         super(6, GeometryArray.COORDINATES, 24); // 6 vertices, 3*8 indices
+
+        //Set the points defined in the assignment
         setCoordinate(0, new Point3f(0f, 0f, 1f));
         setCoordinate(1, new Point3f(-1f, 0f, 0f));
         setCoordinate(2, new Point3f(0f, -1f, 0f));
@@ -19,6 +21,9 @@ public class Octahedron extends IndexedTriangleArray {
         setCoordinate(4, new Point3f(0f, 1f, 0f));
         setCoordinate(5, new Point3f(0f, 0f, -1f));
 
-        int[] coordinates = {};
+        //Here are the coordinates for each point in the 8 triangles
+        int[] coordinates = {0,1,2, 1,2,5, 5,2,3, 3,2,0, 0,4,1, 1,4,5, 5,4,3, 3,4,0};
+
+        setCoordinateIndices(0, coordinates);
     }
 }
